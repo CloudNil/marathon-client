be8842b5d4d0bba7e03ee75b508dbe3620c44426
cloudnil <cloudnil@126.com>
2016-01-05 14:23:01 +0800

    Update

--- src/main/java/cloudnil/marathon/client/model/v2/UpgradeStrategy.java
+++ src/main/java/cloudnil/marathon/client/model/v2/UpgradeStrategy.java
@@ -5,6 +5,15 @@ import cloudnil.marathon.client.utils.ModelUtils;
 public class UpgradeStrategy {
 	private Double minimumHealthCapacity;
 	private Double maximumOverCapacity;
+	
+	public UpgradeStrategy(){
+		
+	}
+	public UpgradeStrategy(Double minimumHealthCapacity, Double maximumOverCapacity) {
+		this.minimumHealthCapacity = minimumHealthCapacity;
+		this.maximumOverCapacity = maximumOverCapacity;
+	}
+	
 	public Double getMinimumHealthCapacity() {
 		return minimumHealthCapacity;
 	}
